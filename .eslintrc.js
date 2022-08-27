3077178798742c170eb8e2322d915ad67b4c844b
Siarhei Startsau <Siarhei_Startsau@epam.com>
2022-08-27 23:20:16 +0200

    chore: update dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,35 +1,35 @@
 module.exports = {
   root: true,
-  extends: ["eslint:recommended"],
+  extends: ['eslint:recommended'],
   env: {
     node: true,
     es6: true
   },
   parserOptions: {
-    ecmaVersion: 2017
+    ecmaVersion: 2022
   },
   rules: {
-    "brace-style": "error",
-    "comma-dangle": "error",
-    "no-caller": "error",
-    "no-extra-bind": "error",
-    "no-extra-semi": "error",
-    "no-process-exit": "error",
-    "no-template-curly-in-string": "error",
-    "no-undef": "error",
-    "no-unsafe-negation": "error",
-    "no-unused-vars": ["error", { args: "none" }],
-    eqeqeq: "error",
-    yoda: "error",
-    indent: "off",
-    "valid-jsdoc": [
-      "error",
+    'brace-style': 'error',
+    'comma-dangle': 'error',
+    'no-caller': 'error',
+    'no-extra-bind': 'error',
+    'no-extra-semi': 'error',
+    'no-process-exit': 'error',
+    'no-template-curly-in-string': 'error',
+    'no-undef': 'error',
+    'no-unsafe-negation': 'error',
+    'no-unused-vars': ['error', { args: 'none' }],
+    eqeqeq: 'error',
+    yoda: 'error',
+    indent: 'off',
+    'valid-jsdoc': [
+      'error',
       {
         prefer: {
-          return: "returns"
+          return: 'returns'
         },
         preferType: {
-          "*": "any"
+          '*': 'any'
         },
         requireReturnType: true
       }
